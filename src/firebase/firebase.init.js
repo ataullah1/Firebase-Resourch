b767f74b4c415c71bd5477b03c500891c038fd2f
Md Ataullah <ataullahm100@gmail.com>
2024-03-30 00:00:47 +0600

    firebase recouch

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTZYHySTQD_b1Wv6lJeIEIAMn-LNMp1CE',
+  authDomain: 'first-firebase-10fcf.firebaseapp.com',
+  projectId: 'first-firebase-10fcf',
+  storageBucket: 'first-firebase-10fcf.appspot.com',
+  messagingSenderId: '468937772711',
+  appId: '1:468937772711:web:b6263541a107892aaf5582',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
