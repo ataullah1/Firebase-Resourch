513882b4c8ae73c6fe8f1bfa61fdc70038719d78
Md Ataullah <ataullahm100@gmail.com>
2024-03-31 09:43:48 +0600

    Login Registe Form Create

--- Practices-3/src/pages/Login/Login.jsx
+++ Practices-3/src/pages/Login/Login.jsx
@@ -1,7 +1,53 @@
 const Login = () => {
   return (
-    <div>
-      <h1>login From</h1>
+    <div className="flex items-center justify-center min-h-[calc(100vh-92px)]">
+      <div className="w-full md:w-9/12 lg:w-1/2 mx-auto border border-gray-300 rounded-lg">
+        {' '}
+        <h1 className="font-bold text-4xl text-center underline py-2">
+          Login Now
+        </h1>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a
+                href="#"
+                className="text-primary label-text-alt link link-hover"
+              >
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              className="btn btn-primary text-white font-semibold text-lg"
+              value="Log In"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- Practices-3/src/pages/Register/Register.jsx
+++ Practices-3/src/pages/Register/Register.jsx
@@ -1,7 +1,54 @@
 const Register = () => {
   return (
-    <div>
-      <h1>This is Register</h1>
+    <div className="flex items-center justify-center min-h-[calc(100vh-92px)]">
+      <div className="w-full md:w-9/12 lg:w-1/2 mx-auto border border-gray-300 rounded-lg">
+        <h1 className="font-bold text-4xl text-center underline py-2">
+          Register Now
+        </h1>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              className="btn btn-primary text-white font-semibold text-lg"
+              value="Register Now"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
