fd06ecbfa94616eefadd962ac6516f859d4a47cb
Md Ataullah <ataullahm100@gmail.com>
2024-04-02 11:40:49 +0600

    Input name add in home section

--- Practices 4/src/pages/Home/Home.jsx
+++ Practices 4/src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../provider/AuthProvider';
+// import { useContext } from 'react';
+// import { AuthContext } from '../../provider/AuthProvider';
+
+import { useEffect, useRef } from 'react';
 
 const Home = () => {
-  const authInf = useContext(AuthContext);
-  console.log(authInf);
+  // const authInf = useContext(AuthContext);
+  // console.log(authInf);
+  const inpRef = useRef();
+  useEffect(() => {
+    inpRef.current.focus();
+  }, []);
   return (
-    <div>
-      <h1 className="text-5xl"> Home Page</h1>
+    <div className="h-[calc(100vh-92px)] flex items-center justify-center">
+      <input
+        ref={inpRef}
+        className="border-2 p-3 min-w-[500px] rounded-sm focus:border-yellow-500 outline-none"
+        type="text"
+        placeholder="Enter Your Name"
+      />
     </div>
   );
 };
