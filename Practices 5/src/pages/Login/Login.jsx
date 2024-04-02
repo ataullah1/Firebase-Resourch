519177d6df1e3714ad0aad46cb2d643b29f4de95
Md Ataullah <ataullahm100@gmail.com>
2024-04-02 17:59:47 +0600

    Form Added

--- Practices 5/src/pages/Login/Login.jsx
+++ Practices 5/src/pages/Login/Login.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
+  const [eye, setEye] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -28,22 +33,41 @@ const Login = () => {
             placeholder="Email"
             {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
           />
-          <input
-            className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
-            type="password"
-            placeholder="Password"
-            {...register('Password', {
-              required: true,
-              minLength: 6,
-              maxLength: 50,
-            })}
-          />
-
+          <div className="relative">
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type={eye ? 'text' : 'password'}
+              placeholder="Password"
+              {...register('Password', {
+                required: true,
+                minLength: 6,
+                maxLength: 50,
+              })}
+            />
+            <span
+              className="absolute top-1/2 -translate-y-1/2 right-4 text-xl cursor-pointer"
+              onClick={() => setEye(!eye)}
+            >
+              {eye ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>
+              <input type="checkbox" /> <label>Remember me</label>
+            </div>
+            <p className="underline cursor-pointer">Forgot Password</p>
+          </div>
           <button className="relative group w-full py-2 px-4 border-2 font-bold tracking-widest active:scale-95 duration-150 hover:border-purple-600 hover:text-purple-600 border-orange-400 rounded">
             <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-indigo-600 group-hover:w-full group-hover:transition-all"></span>
             Log In
           </button>
         </form>
+        <p className="pt-2">
+          Don't have an account? &nbsp;
+          <Link to={'/register'} className="underline">
+            Create Account
+          </Link>
+        </p>
       </div>
     </div>
   );
--- Practices 5/src/pages/Register/Register.jsx
+++ Practices 5/src/pages/Register/Register.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [eye, setEye] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -10,45 +15,89 @@ const Register = () => {
   console.log(errors);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="First name"
-        {...register('First name', { required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Last name"
-        {...register('Last name', { required: true, maxLength: 100 })}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register('Mobile number', {
-          required: true,
-          minLength: 6,
-          maxLength: 12,
-        })}
-      />
-
-      <input
-        {...register('Developer', { required: true })}
-        type="radio"
-        value="Yes"
-      />
-      <input
-        {...register('Developer', { required: true })}
-        type="radio"
-        value="No"
-      />
-
-      <input type="submit" />
-    </form>
+    <div className="min-h-[calc(100vh-92px)] flex items-center justify-center">
+      <div className="border-2 border-orange-400 rounded-md p-8 bg-gradient-to-r from-emerald-400 to-cyan-400 w-full lg:w-2/3">
+        <h1 className="font-bold text-center text-3xl lg:text-6xl">
+          Create Account
+        </h1>
+        <form className="mt-8 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col md:flex-row justify-between gap-4">
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type="text"
+              placeholder="First Name"
+              {...register('First Name', { required: true, maxLength: 100 })}
+            />
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type="text"
+              placeholder="Last Name"
+              {...register('Last Name', { required: true, maxLength: 100 })}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row justify-between gap-4">
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type="text"
+              placeholder="Username"
+              {...register('userName', { required: true, maxLength: 100 })}
+            />
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type="number"
+              placeholder="Number"
+              {...register('Number', { required: true, maxLength: 100 })}
+            />
+          </div>
+          <input
+            className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+            type="email"
+            placeholder="Email"
+            {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+          />
+          <div className="flex flex-col md:flex-row justify-between gap-4">
+            <div className="w-full relative">
+              <span
+                className="absolute top-1/2 -translate-y-1/2 right-4 text-xl cursor-pointer"
+                onClick={() => setEye(!eye)}
+              >
+                {eye ? <FaEyeSlash /> : <FaEye />}
+              </span>
+              <input
+                className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+                type={eye ? 'text' : 'password'}
+                placeholder="Password"
+                {...register('Password', {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 50,
+                })}
+              />
+            </div>
+            <input
+              className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+              type="password"
+              placeholder="Confirm Password"
+              {...register('Confirm Password', {
+                required: true,
+                minLength: 6,
+                maxLength: 50,
+              })}
+            />
+          </div>
+          <button className="relative group w-full py-2 px-4 border-2 font-bold tracking-widest active:scale-95 duration-150 hover:border-purple-600 hover:text-purple-600 border-orange-400 rounded">
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-indigo-600 group-hover:w-full group-hover:transition-all"></span>
+            Create Account
+          </button>
+        </form>
+        <p className="pt-2">
+          Already have an account?{' '}
+          <Link to={'/login'} className="underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 };
 export default Register;
