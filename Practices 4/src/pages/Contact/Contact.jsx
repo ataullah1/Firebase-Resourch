52485c57ac2211f15df4408491a5df8c5b99a494
Md Ataullah <ataullahm100@gmail.com>
2024-04-02 13:03:31 +0600

    Requard true and massage addign

--- Practices 4/src/pages/Contact/Contact.jsx
+++ Practices 4/src/pages/Contact/Contact.jsx
@@ -5,10 +5,17 @@ const Contact = () => {
     register,
     handleSubmit,
     // watch,
-    // formState: { errors },
-  } = useForm();
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: 'Md',
+      lastName: 'Ataullah',
+    },
+  });
 
   const onSubmit = (data) => console.log(data);
+
+  // console.log(errors);
   return (
     <div className="h-[calc(100vh-92px)] flex items-center justify-center">
       <div className="w-full lg:w-3/5 mx-auto">
@@ -22,16 +29,32 @@ const Contact = () => {
               className="border border-gray-500 rounded px-4 py-2 w-full"
               placeholder="First Name"
               name="firstName"
-              {...register('firstName')}
+              {...register('firstName', {
+                required: {
+                  value: true,
+                  message: 'You must fill the First Name Input.',
+                },
+              })}
             />
             <input
               type="text"
               className="border border-gray-500 rounded px-4 py-2 w-full"
               placeholder="Last Name"
               name="lastName"
-              {...register('lastName')}
+              {...register('lastName', {
+                required: {
+                  value: true,
+                  message: 'You must fill the Last Name Input.',
+                },
+              })}
             />
           </div>
+          {errors.firstName && (
+            <p className="text-red-500">{errors.firstName.message}</p>
+          )}
+          {errors.lastName && (
+            <p className="text-red-500">{errors.lastName.message}</p>
+          )}
           <input
             type="submit"
             value="Register"
