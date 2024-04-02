28d9e9b508b8a3ac61058c690ab7a465a7faf8c5
Md Ataullah <ataullahm100@gmail.com>
2024-04-02 17:27:17 +0600

    Login Form Added:

--- Practices 5/src/pages/Login/Login.jsx
+++ Practices 5/src/pages/Login/Login.jsx
@@ -1,9 +1,51 @@
+import { useForm } from 'react-hook-form';
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <div>
-      <h1 className="text-6xl font-bold text-center">Login pafge</h1>
+    <div className="min-h-[calc(100vh-92px)] flex items-center justify-center">
+      <div className="border-2 border-orange-400 rounded-md p-8 bg-gradient-to-r from-teal-400 to-yellow-200">
+        <h1 className="font-bold text-center text-3xl lg:text-6xl">
+          Login Account
+        </h1>
+        <form className="mt-8 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+            type="text"
+            placeholder="Username"
+            {...register('userName', { required: true, maxLength: 100 })}
+          />
+          <input
+            className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+            type="email"
+            placeholder="Email"
+            {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+          />
+          <input
+            className="w-full py-2 px-4 border border-orange-400 rounded outline-none focus:border-purple-600 duration-150"
+            type="password"
+            placeholder="Password"
+            {...register('Password', {
+              required: true,
+              minLength: 6,
+              maxLength: 50,
+            })}
+          />
+
+          <button className="relative group w-full py-2 px-4 border-2 font-bold tracking-widest active:scale-95 duration-150 hover:border-purple-600 hover:text-purple-600 border-orange-400 rounded">
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-indigo-600 group-hover:w-full group-hover:transition-all"></span>
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default Login;
--- Practices 5/src/pages/Register/Register.jsx
+++ Practices 5/src/pages/Register/Register.jsx
@@ -1,9 +1,54 @@
+import { useForm } from 'react-hook-form';
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <div>
-      <h1 className="text-6xl font-bold text-center">Register pafge</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="First name"
+        {...register('First name', { required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="Last name"
+        {...register('Last name', { required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="tel"
+        placeholder="Mobile number"
+        {...register('Mobile number', {
+          required: true,
+          minLength: 6,
+          maxLength: 12,
+        })}
+      />
+
+      <input
+        {...register('Developer', { required: true })}
+        type="radio"
+        value="Yes"
+      />
+      <input
+        {...register('Developer', { required: true })}
+        type="radio"
+        value="No"
+      />
+
+      <input type="submit" />
+    </form>
   );
 };
-
 export default Register;
