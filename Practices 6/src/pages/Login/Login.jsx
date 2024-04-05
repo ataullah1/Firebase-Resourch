acaf6a032920af76b4e92dbf257a9394cde8f241
Md Ataullah <ataullahm100@gmail.com>
2024-04-05 10:05:30 +0600

    Form added SSignup

--- Practices 6/src/pages/Login/Login.jsx
+++ Practices 6/src/pages/Login/Login.jsx
@@ -1,59 +1,52 @@
+import { useState } from 'react';
+import { FaEye, FaEyeSlash, FaUserAlt } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { TbPasswordFingerprint } from 'react-icons/tb';
 const Login = () => {
+  const [eye, setEye] = useState(false);
   return (
-    <div>
-      <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Search" />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clipRule="evenodd"
+    <div className="flex items-center justify-center min-h-[calc(100vh-92px)]">
+      <div className="w-full md:w-3/4 lg:w-1/2 mx-auto">
+        <h1 className="text-5xl text-center font-bold pb-6">Register Now</h1>
+        <form className="space-y-4">
+          <label className="input input-bordered flex items-center gap-2">
+            <FaUserAlt />
+            <input type="text" className="grow" placeholder="Your Name" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <MdEmail />
+            <input type="email" className="grow" placeholder="Email" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <RiLockPasswordFill />
+            <input
+              type={eye ? 'text' : 'password'}
+              className="grow"
+              placeholder="Password"
+            />
+            <div
+              onClick={() => setEye(!eye)}
+              className="cursor-pointer text-xl"
+            >
+              {eye ? <FaEyeSlash /> : <FaEye />}
+            </div>
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <TbPasswordFingerprint />
+            <input
+              type="password"
+              className="grow"
+              placeholder="Confirm Password"
+            />
+          </label>
+          <input
+            type="submit"
+            value="Register"
+            className="w-full py-2 px-4 rounded-md text-center text-white font-bold bg-secondary"
           />
-        </svg>
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-          <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-        </svg>
-        <input type="text" className="grow" placeholder="Email" />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
-        <input type="text" className="grow" placeholder="Username" />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <input type="password" className="grow" value="password" />
-      </label>
+        </form>
+      </div>
     </div>
   );
 };
