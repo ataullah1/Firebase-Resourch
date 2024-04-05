d4320cd3f821a37504f4eae2ccdeda02ee053b23
Md Ataullah <ataullahm100@gmail.com>
2024-04-05 10:30:21 +0600

    Login From Added: '

--- Practices 6/src/pages/Login/Login.jsx
+++ Practices 6/src/pages/Login/Login.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react';
-import { FaEye, FaEyeSlash, FaImage, FaUserAlt } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri';
-import { TbPasswordFingerprint } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
+import imageLogin from '../../assets/Login.jpg';
 const Login = () => {
   const [eye, setEye] = useState(false);
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-92px)]">
       <div className="flex flex-col md:flex-row-reverse items-center w-full gap-5">
-        <div className="w-full md:w-1/2 bg-red-200 h-44 "></div>
+        <div className="w-full md:w-1/2 bg-red-200 h-full ">
+          <img src={imageLogin} alt="" className="h-full" />
+        </div>
         <div className="w-full md:w-1/2 mx-auto border-2 rounded-lg p-5">
-          <h1 className="text-3xl font-bold pb-6">Create an account</h1>
-          <form className="space-y-4">
-            <label className="input input-bordered flex items-center gap-2">
-              <FaUserAlt />
-              <input type="text" className="grow" placeholder="Your Name" />
-            </label>
-            <label className="input input-bordered flex items-center gap-2">
-              <FaImage />
-              <input
-                type="text"
-                className="grow"
-                placeholder="Your Image URL"
-              />
-            </label>
-            <label className="input input-bordered flex items-center gap-2">
-              <FaUserAlt />
-              <input type="text" className="grow" placeholder="Your Name" />
-            </label>
+          <h1 className="text-3xl font-bold pb-6">Login Your Account</h1>
+          <form className="space-y-6">
             <label className="input input-bordered flex items-center gap-2">
               <MdEmail />
               <input type="email" className="grow" placeholder="Email" />
@@ -47,21 +33,20 @@ const Login = () => {
                 {eye ? <FaEyeSlash /> : <FaEye />}
               </div>
             </label>
-            <label className="input input-bordered flex items-center gap-2">
-              <TbPasswordFingerprint />
-              <input
-                type="password"
-                className="grow"
-                placeholder="Confirm Password"
-              />
-            </label>
+            <div className="flex justify-between items-center">
+              <div>
+                <input type="checkbox" className="cursor-pointer" />{' '}
+                <label>Remember me</label>
+              </div>
+              <p className="underline cursor-pointer">Forgot Password</p>
+            </div>
             <input
               type="submit"
-              value="Register"
-              className="w-full py-2 px-4 rounded-md text-center text-white font-bold bg-secondary"
+              value="Login"
+              className="w-full py-2 px-4 rounded-md text-center text-white font-bold bg-secondary active:scale-95 duration-150"
             />
           </form>{' '}
-          <p className="pt-2">
+          <p className="pt-3">
             {`Don't have an account? `}
             <Link to={'/register'} className="underline">
               Create Account
